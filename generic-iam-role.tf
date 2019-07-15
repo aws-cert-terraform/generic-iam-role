d4b470970bb0d45a1cffb81fac7a76b11e052556
Ian Cullinane <ian.cullinane@gmail.com>
2019-07-14 22:05:48 -0400

    add base name to iam

--- generic-iam-role.tf
+++ generic-iam-role.tf
@@ -28,6 +28,7 @@ data "aws_iam_policy_document" "instance-assume-role-policy" {
 }
 
 resource "aws_iam_role" "general_access" {
+  name = "testiam"
   assume_role_policy = "${data.aws_iam_policy_document.instance-assume-role-policy.json}"
 }
 
