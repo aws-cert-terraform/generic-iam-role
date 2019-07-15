5086799f92d8bde980f68aedb807bc881fec0994
Ian Cullinane <ian.cullinane@gmail.com>
2019-07-14 21:51:07 -0400

    remove faulty name param

--- generic-iam-role.tf
+++ generic-iam-role.tf
@@ -28,7 +28,7 @@ data "aws_iam_policy_document" "instance-assume-role-policy" {
 }
 
 resource "aws_iam_role" "general_access" {
-  name               = "generic_role"
+  // name               = "generic_role"
   assume_role_policy = "${data.aws_iam_policy_document.instance-assume-role-policy.json}"
 }
 
